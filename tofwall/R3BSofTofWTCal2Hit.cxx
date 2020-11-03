aabacfb275ffc5d07027c3897b25e5e0e230106c
Ryo Taniuchi <ryo.taniuchi@york.ac.uk>
2020-11-03 12:41:57 +0100

    SofTofWHitPar is now in use and calibrated.

--- tofwall/R3BSofTofWTCal2Hit.cxx
+++ tofwall/R3BSofTofWTCal2Hit.cxx
@@ -20,76 +20,15 @@
 #include "R3BSofTofWSingleTcalData.h"
 #include "R3BSofTofWTCal2Hit.h"
 
-// FIXME after beam 15 Feb 2020
-Double_t offset_tof[28];
-Double_t offset_pos[28];
-
 // R3BSofTofWTCal2Hit: Default Constructor --------------------------
 R3BSofTofWTCal2Hit::R3BSofTofWTCal2Hit()
     : FairTask("R3BSof TofW tcal2hit Task", 1)
     , fTCalDataCA(NULL)
     , fHitDataCA(NULL)
     , fOnline(kFALSE)
+    , Tof_lise(43.)
+    , TofWPosition(560.)
 {
-    offset_pos[0] = +2.28;
-    offset_pos[1] = +1.03;
-    offset_pos[2] = +1.287;
-    offset_pos[3] = -2.03;
-    offset_pos[4] = -0.62;
-    offset_pos[5] = -0.50;
-    offset_pos[6] = +0.81;
-    offset_pos[7] = -0.16;
-    offset_pos[8] = +3.531;
-    offset_pos[9] = +2.17;
-    offset_pos[10] = -1.67;
-    offset_pos[11] = -1.26;
-    offset_pos[12] = +2.265;
-    offset_pos[13] = +4.42;
-    offset_pos[14] = +6.51;
-    offset_pos[15] = +0.14;
-    offset_pos[16] = -2.21;
-    offset_pos[17] = -6.15;
-    offset_pos[18] = -0.45;
-    offset_pos[19] = +0.47;
-    offset_pos[20] = +1.25;
-    offset_pos[21] = +3.09;
-    offset_pos[22] = -1.07;
-    offset_pos[23] = +2.44;
-    offset_pos[24] = +0.495;
-    offset_pos[25] = -0.22;
-    offset_pos[26] = +0.93;
-    offset_pos[27] = 0.0;
-
-    // Double_t Tof_lise = 41.6; // ns
-
-    offset_tof[0] = Tof_lise + 37.08;
-    offset_tof[1] = Tof_lise + 36.43;
-    offset_tof[2] = Tof_lise + 37.74;
-    offset_tof[3] = Tof_lise + 36.86;
-    offset_tof[4] = Tof_lise + 37.32;
-    offset_tof[5] = Tof_lise + 37.63;
-    offset_tof[6] = Tof_lise + 38.05;
-    offset_tof[7] = Tof_lise + 37.68;
-    offset_tof[8] = Tof_lise + 36.8;
-    offset_tof[9] = Tof_lise + 37.59;
-    offset_tof[10] = Tof_lise + 35.88;
-    offset_tof[11] = Tof_lise + 37.58;
-    offset_tof[12] = Tof_lise + 36.36;
-    offset_tof[13] = Tof_lise + 37.14;
-    offset_tof[14] = Tof_lise + 38.94;
-    offset_tof[15] = Tof_lise + 38.6;
-    offset_tof[16] = Tof_lise + 39.29;
-    offset_tof[17] = Tof_lise + 38.14;
-    offset_tof[18] = Tof_lise + 38.72;
-    offset_tof[19] = Tof_lise + 38.48;
-    offset_tof[20] = Tof_lise + 40.33;
-    offset_tof[21] = Tof_lise + 40.53;
-    offset_tof[22] = Tof_lise + 40.13;
-    offset_tof[23] = Tof_lise + 40.15;
-    offset_tof[24] = Tof_lise + 39.51;
-    offset_tof[25] = Tof_lise + 39.3;
-    offset_tof[26] = Tof_lise + 39.33;
-    offset_tof[27] = Tof_lise;
 }
 
 // R3BSofTofWTCal2Hit: Standard Constructor --------------------------
@@ -98,66 +37,9 @@ R3BSofTofWTCal2Hit::R3BSofTofWTCal2Hit(const char* name, Int_t iVerbose)
     , fTCalDataCA(NULL)
     , fHitDataCA(NULL)
     , fOnline(kFALSE)
+    , Tof_lise(43.)
+    , TofWPosition(560.)
 {
-    offset_pos[0] = +2.28;
-    offset_pos[1] = +1.03;
-    offset_pos[2] = +1.287;
-    offset_pos[3] = -2.03;
-    offset_pos[4] = -0.62;
-    offset_pos[5] = -0.50;
-    offset_pos[6] = +0.81;
-    offset_pos[7] = -0.16;
-    offset_pos[8] = +3.531;
-    offset_pos[9] = +2.17;
-    offset_pos[10] = -1.67;
-    offset_pos[11] = -1.26;
-    offset_pos[12] = +2.265;
-    offset_pos[13] = +4.42;
-    offset_pos[14] = +6.51;
-    offset_pos[15] = +0.14;
-    offset_pos[16] = -2.21;
-    offset_pos[17] = -6.15;
-    offset_pos[18] = -0.45;
-    offset_pos[19] = +0.47;
-    offset_pos[20] = +1.25;
-    offset_pos[21] = +3.09;
-    offset_pos[22] = -1.07;
-    offset_pos[23] = +2.44;
-    offset_pos[24] = +0.495;
-    offset_pos[25] = -0.22;
-    offset_pos[26] = +0.93;
-    offset_pos[27] = 0.0;
-
-    // Double_t Tof_lise = 43.0; // ns
-
-    offset_tof[0] = Tof_lise + 37.08;
-    offset_tof[1] = Tof_lise + 36.43;
-    offset_tof[2] = Tof_lise + 37.74;
-    offset_tof[3] = Tof_lise + 36.86;
-    offset_tof[4] = Tof_lise + 37.32;
-    offset_tof[5] = Tof_lise + 37.63;
-    offset_tof[6] = Tof_lise + 38.05;
-    offset_tof[7] = Tof_lise + 37.68;
-    offset_tof[8] = Tof_lise + 36.8;
-    offset_tof[9] = Tof_lise + 37.59;
-    offset_tof[10] = Tof_lise + 35.88;
-    offset_tof[11] = Tof_lise + 37.58;
-    offset_tof[12] = Tof_lise + 36.36;
-    offset_tof[13] = Tof_lise + 37.14;
-    offset_tof[14] = Tof_lise + 38.94;
-    offset_tof[15] = Tof_lise + 38.6;
-    offset_tof[16] = Tof_lise + 39.29;
-    offset_tof[17] = Tof_lise + 38.14;
-    offset_tof[18] = Tof_lise + 38.72;
-    offset_tof[19] = Tof_lise + 38.48;
-    offset_tof[20] = Tof_lise + 40.33;
-    offset_tof[21] = Tof_lise + 40.53;
-    offset_tof[22] = Tof_lise + 40.13;
-    offset_tof[23] = Tof_lise + 40.15;
-    offset_tof[24] = Tof_lise + 39.51;
-    offset_tof[25] = Tof_lise + 39.3;
-    offset_tof[26] = Tof_lise + 39.33;
-    offset_tof[27] = Tof_lise;
 }
 
 // Virtual R3BSofTofWTCal2Hit: Destructor
@@ -168,6 +50,20 @@ R3BSofTofWTCal2Hit::~R3BSofTofWTCal2Hit()
         delete fTCalDataCA;
     if (fHitDataCA)
         delete fHitDataCA;
+}
+
+void R3BSofTofWTCal2Hit::SetParContainers()
+{
+    fTofWHitPar = (R3BSofTofWHitPar*)FairRuntimeDb::instance()->getContainer("tofwHitPar");
+    if (!fTofWHitPar)
+    {
+        LOG(ERROR) << "R3BSofTofWTCal2Hit::SetParContainers() : Could not get access to tofwHitPar-Container.";
+        return;
+    }
+    else
+        LOG(INFO) << "R3BSofTofWTCal2Hit::SetParContainers() : tofwHitPar-Container found with "
+                  << fTofWHitPar->GetNumSci() << " paddles.";
+    fTofWHitPar->printParams();
 }
 
 // -----   Public method Init   --------------------------------------------
@@ -215,26 +111,33 @@ void R3BSofTofWTCal2Hit::Exec(Option_t* option)
 
     // Reading the Input -- Cal Data --
     Int_t nHits = fTCalDataCA->GetEntries();
-    if (!nHits)
+    if (nHits < 1)
         return;
 
     // Data from cal level
     R3BSofTofWSingleTcalData** calDat;
     calDat = new R3BSofTofWSingleTcalData*[nHits];
     Int_t fPaddleId = 0; // from 1 to 28
-    Double_t tofw = 0.;
+    Double_t tofw = 0., posy = 0.;
+    Int_t mult = 0;
 
-    if (nHits == 1)
-        for (Int_t i = 0; i < nHits; i++)
-        {
-            calDat[i] = (R3BSofTofWSingleTcalData*)(fTCalDataCA->At(i));
-            fPaddleId = calDat[i]->GetDetector();
-            tofw = calDat[i]->GetRawTofNs();
-            AddHitData(fPaddleId,
-                       -450. + 15. + (Double_t)(fPaddleId - 1) * 30.,
-                       calDat[i]->GetRawPosNs() + offset_pos[fPaddleId - 1],
-                       tofw + offset_tof[fPaddleId - 1]);
-        }
+    for (Int_t i = 0; i < nHits; i++)
+    {
+        calDat[i] = (R3BSofTofWSingleTcalData*)(fTCalDataCA->At(i));
+        fPaddleId = calDat[i]->GetDetector();
+        if (fTofWHitPar->GetInUse(fPaddleId) != 1)
+            continue;
+        mult++;
+        tofw = calDat[i]->GetRawTofNs();
+        posy = calDat[i]->GetRawPosNs();
+    }
+
+    if (mult == 1)
+        AddHitData(
+            fPaddleId,
+            (TofWPosition - 525.) - 14. * 30. + (Double_t)(fPaddleId - 1) * 30., // x=0 at the gap of bars 14 and 15
+            posy - fTofWHitPar->GetPosPar(fPaddleId),
+            tofw - fTofWHitPar->GetTofPar((Int_t)fPaddleId) + Tof_lise);
 
     if (calDat)
         delete calDat;
--- tofwall/R3BSofTofWTCal2Hit.h
+++ tofwall/R3BSofTofWTCal2Hit.h
@@ -9,6 +9,7 @@
 #include "FairTask.h"
 #include "R3BSofTofWTcalData.h"
 #include "R3BSofTofWHitData.h"
+#include "R3BSofTofWHitPar.h"
 #include "TH1F.h"
 #include <TRandom.h>
 
@@ -34,6 +35,8 @@ class R3BSofTofWTCal2Hit : public FairTask
     virtual void Reset();
 
     // Fair specific
+    virtual void SetParContainers();
+
     /** Virtual method Init **/
     virtual InitStatus Init();
 
@@ -45,14 +48,18 @@ class R3BSofTofWTCal2Hit : public FairTask
 
     void SetOnline(Bool_t option) { fOnline = option; }
     void SetTofLISE(Double_t tof) {Tof_lise = tof;}
+    void SetTofWPos(Double_t pos) {TofWPosition = pos;}
     Double_t GetTofLISE() {return Tof_lise;}
-    
+    Double_t GetTofWPos() {return TofWPosition;}
+        
   private:
     Bool_t fOnline; // Don't store data for online
 
     TClonesArray* fTCalDataCA; /**< Array with Cal input data. >*/
     TClonesArray* fHitDataCA;  /**< Array with Hit output data. >*/
-    Double_t Tof_lise = 43.;
+    Double_t Tof_lise;// = 43.;
+    Double_t TofWPosition;// = 560.
+    R3BSofTofWHitPar* fTofWHitPar;
     
     /** Private method AddHitData **/
     // Adds a SofTofWHitData to the HitCollection
